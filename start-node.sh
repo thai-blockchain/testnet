2f683701ac75dcd45e56e601cf2ff8a80fef7c1a
Kitt Tientanopajai <kitt@kku.ac.th>
2022-07-29 17:21:42 +0700

    Initial commit

---
+++ start-node.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+docker run -dit --restart=unless-stopped \
+	--name testnet-xxx \
+	-v $PWD:/srv \
+	-v $PWD/static-nodes.json:/srv/node/geth/static-nodes.json:ro \
+	-w /srv \
+	-p 30303:30303 \
+	ethereum/client-go:stable \
+	--networkid 1708 \
+	--port 30303 \
+	--datadir /srv/node \
+	--unlock abcdef0123456789abcdef0123456789abcdef01 \
+	--password password.txt \
+	--gcmode archive \
+	--syncmode full \
+	--mine \
+	--nousb \
+	--nodiscover
